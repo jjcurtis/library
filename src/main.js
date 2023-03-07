c9ac17134465bfe0a85e3ee8458693be1ac7d6df
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-03-07 10:58:43 -0600

    Create user input for books

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
+/* eslint-disable no-sequences */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-alert */
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
+const submit = document.getElementById('submit');
 
+const library = [];
+
+function Book() {
+  (this.title = title.value),
+    (this.author = author.value),
+    (this.pages = pages.value),
+    (this.read = read.value);
+}
+
+function addToLibrary() {
+  if (
+    title.value === '' ||
+    author.value === '' ||
+    pages.value === '' ||
+    read.value === ''
+  ) {
+    alert('Please complete each field to submit a new book');
+    return;
+  }
+
+  library.push(new Book());
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.value = '';
+}
+
+submit.addEventListener('click', e => {
+  e.preventDefault();
+  addToLibrary();
+});
