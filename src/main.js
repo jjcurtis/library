56fd583ba6233f238ca25069066e4cd6bc000010
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-03-08 08:25:27 -0600

    Make function to create dynamic cards for books

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-sequences */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-alert */
+const body = document.querySelector('body');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('read');
 const submit = document.getElementById('submit');
+
+const div = document.createElement('div');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
 
 const library = [];
 
@@ -34,7 +39,27 @@ function addToLibrary() {
   read.value = '';
 }
 
+function createCard() {
+  div.classList = 'card';
+  ul.classList = 'list';
+
+  body.append(div);
+  div.append(ul);
+
+  const list = document.querySelector('.list');
+  const newBook = library[library.length - 1];
+  const keys = Object.keys(newBook);
+  const values = Object.values(newBook);
+
+  for (let i = 0; i < keys.length; i++) {
+    li.classList = `${keys[i]}`;
+    li.textContent = `${values[i]}`;
+    list.appendChild(li.cloneNode(true));
+  }
+}
+
 submit.addEventListener('click', e => {
   e.preventDefault();
   addToLibrary();
+  createCard();
 });
